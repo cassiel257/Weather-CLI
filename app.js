1d9b1127606b03ee7795bfc9f30dc04e7f88ded0
Tiffany Udoh <tiffany.udoh@gmail.com>
2021-04-15 21:42:14 -0400

    added sample mapbox api data

--- app.js
+++ app.js
@@ -5,9 +5,17 @@ console.log(greeting);
 const http = require('http');
 const fetch = require('node-fetch');
 
-const url = 'http://api.open-notify.org/astros.json';
+const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/paris.json?access_token=pk.eyJ1IjoiaW5maW5pdGV1c2VybmFtZSIsImEiOiJja25qbjY3b2YwMDJvMnVxdmtqcmE5MHM2In0.Ke1ijexIWlsMVAF4LeVIzg';
 
 fetch(url).then((response)=>{
+    return (response.json());
+}).then((data=>{
+    console.log(data);
+}));
+
+const url2 = 'https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,minutely&appid=852e338b96d2abd5afdb4e3bf6f207d5';
+
+fetch(url2).then((response)=>{
     return (response.json());
 }).then((data=>{
     console.log(data);
